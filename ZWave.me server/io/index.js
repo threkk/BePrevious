5e5d9b0d187789c0bdb4c7fca8d51ae039fe7f63
Bjorn Water <bjorn.water@gmail.com>
2013-04-02 13:24:09 +0200

    - Added moment for time
    - created empty file method
    - created write method

--- ZWave.me server/io/index.js
+++ ZWave.me server/io/index.js
@@ -1,32 +1,45 @@
 var exec = require('child_process').exec;
-
+var fs = require("fs");
 var cronJob = require('cron').CronJob;
-
+var moment = require('moment');
 var job = new cronJob({
-    cronTime: '*/5 * * * * *',
+    cronTime: '00 02 12 * * 1-5',
     onTick: function () {
-    	createTar('input.txt');
-    	
+        createTar('input.txt');
     }
 });
 startJob();
 
 function startJob() {
-	job.start();
-}
-
-function endJob() {
-	job.stop();
+    job.start();
 }
 
 
 function createTar(filename) {
-    var child = exec("tar -zcf test.tgz " + filename, function (error, stdout, stderr) {
+    var child = exec('tar -zcf ' + returnDate() + '.tgz ' + filename, function (error, stdout, stderr) {
         console.log('execute');
         console.log(stdout);
         console.log(stderr);
-        if (error) {
-            console.log('exec error: ' + error);
+        if (!error) {
+            emptyFile();
         }
     });
+}
+
+function returnDate() {
+    var now = moment().format('ll');
+    return now;
+}
+
+function emptyFile() {
+    fs.writeFile('input.txt', '', function (err) {
+        if (err) console.log(err);
+    });
+}
+
+function writeToFile(text) {
+    var log = fs.createWriteStream('input.txt', {
+        'flags': 'a'
+    });
+    log.end("this is a message");
 }
