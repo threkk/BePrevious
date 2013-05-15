58603d6107730eab07c48d0bb368ca73a3ff2822
Bjorn Water <bjorn.water@gmail.com>
2013-05-15 14:09:08 +0200

    Fixed the edit device when no form

--- ZWave.me server/public/js/editdevice.js
+++ ZWave.me server/public/js/editdevice.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
-	$("#editForm").submit(function(e) {		
-		var deviceId = $("#deviceId").val()
-		var timeout = $("#inputTimeout").val();
-		var url = 'http://localhost:8083/ZWaveAPI/Run/devices['+deviceId+'].instances[0].commandClasses[0x84].Set('+timeout+',1)'
-		$.post(url)
-			.done(function(data) {
-				alert("Data Loaded: " + data);
-					});
-   		return false;
-	});
+    $("#editForm").submit(function (e) {
+        var inputTimeout = $("#inputTimeout");
+        if (inputTimeout.length != 0) {
+            var deviceId = $("#deviceId").val()
+            var timeout = $("#inputTimeout").val();
+            var url = 'http://localhost:8083/ZWaveAPI/Run/devices[' + deviceId + '].instances[0].commandClasses[0x84].Set(' + timeout + ',1)'
+            $.post(url)
+                .done(function (data) {
+                alert("Data Loaded: " + data);
+            });
+
+        }
+        return false;
+    });
 });
