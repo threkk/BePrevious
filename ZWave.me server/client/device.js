61a885ba28afc62a6383571e455e665ce21dbb0e
Bjorn Water <bjorn.water@gmail.com>
2013-05-15 12:19:59 +0200

    - added multilevel support to be able to read temp and luminescence

--- ZWave.me server/client/device.js
+++ ZWave.me server/client/device.js
@@ -17,17 +17,21 @@ function Device(manager) {
         hasWakeup: false,
         hasBattery: false,
         isFailed: false,
+        multiLevel: {},
         description: {}
     }
+
+
 }
 
 
 Device.prototype = {
     update: function (deviceData) {
         this._merge(this.data, deviceData);
+        this.updateMultiLevel();
     },
 
-    _merge: function(oldData, newData) {
+    _merge: function (oldData, newData) {
         var dirty = false;
         for (var property in newData) {
             var oldValue = oldData[property];
@@ -46,6 +50,34 @@ Device.prototype = {
             }
         }
         return dirty
+    },
+
+    updateMultiLevel: function () {
+        var command = 'devices[' + this.data.id + '].instances[0].commandClasses[49]';
+		var self = this;
+        this.manager.client.runCommand(command, function (err, json) {
+            if (err) {
+                return logger.error(err);
+            }
+            logger.debug('json: ' + JSON.stringify(json));
+            if (!json) {
+                return;
+            }
+
+            var data = json.data;
+
+            var multilevel = {};
+            for (var key in json.data) {
+                var instance = json.data[key];
+                if (instance && instance.sensorTypeString) {
+
+                    var sensorType = instance.sensorTypeString.value;
+                    multilevel[sensorType] = instance.val.value;
+                }
+            
+            }
+            self._merge(self.data, {multilevel:multilevel});
+        });
     }
 };
 
