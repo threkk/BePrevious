bc92221022476e9238f63af062d238d5e5d4951c
terraego <terraego@gmail.com>
2013-09-20 11:17:33 +0200

    - removed binary files, and generated resources

--- BoxLabApp/gen/com/hva/boxlabapp/R.java
+++ BoxLabApp/gen/com/hva/boxlabapp/R.java
@@ -9,67 +9,6 @@ package com.hva.boxlabapp;
 
 public final class R {
     public static final class attr {
-        /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
-<p>This may also be a reference to a resource (in the form
-"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
-theme attribute (in the form
-"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
-containing a value of this type.
-         */
-        public static final int state_current_month=0x7f010001;
-        /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
-<p>This may also be a reference to a resource (in the form
-"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
-theme attribute (in the form
-"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
-containing a value of this type.
-         */
-        public static final int state_range_first=0x7f010003;
-        /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
-<p>This may also be a reference to a resource (in the form
-"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
-theme attribute (in the form
-"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
-containing a value of this type.
-         */
-        public static final int state_range_last=0x7f010005;
-        /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
-<p>This may also be a reference to a resource (in the form
-"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
-theme attribute (in the form
-"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
-containing a value of this type.
-         */
-        public static final int state_range_middle=0x7f010004;
-        /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
-<p>This may also be a reference to a resource (in the form
-"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
-theme attribute (in the form
-"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
-containing a value of this type.
-         */
-        public static final int state_selectable=0x7f010000;
-        /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
-<p>This may also be a reference to a resource (in the form
-"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
-theme attribute (in the form
-"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
-containing a value of this type.
-         */
-        public static final int state_today=0x7f010002;
-    }
-    public static final class color {
-        public static final int calendar_active_month_bg=0x7f040000;
-        public static final int calendar_bg=0x7f040001;
-        public static final int calendar_divider=0x7f040002;
-        public static final int calendar_inactive_month_bg=0x7f040003;
-        public static final int calendar_selected_day_bg=0x7f040004;
-        public static final int calendar_selected_range_bg=0x7f040005;
-        public static final int calendar_text_active=0x7f040007;
-        public static final int calendar_text_inactive=0x7f040006;
-        public static final int calendar_text_selected=0x7f040008;
-        public static final int calendar_text_selector=0x7f04000a;
-        public static final int calendar_text_unselectable=0x7f040009;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -78,15 +17,11 @@ containing a value of this type.
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050005;
-        public static final int activity_vertical_margin=0x7f050006;
-        public static final int calendar_day_headers_paddingbottom=0x7f050000;
-        public static final int calendar_month_title_bottommargin=0x7f050002;
-        public static final int calendar_month_topmargin=0x7f050001;
-        public static final int calendar_text_medium=0x7f050003;
-        public static final int calendar_text_small=0x7f050004;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
+<<<<<<< Updated upstream
         public static final int calendar_bg_selector=0x7f020000;
         public static final int earthtruecolor_nasa_big=0x7f020001;
         public static final int ic_action_about=0x7f020002;
@@ -114,27 +49,45 @@ containing a value of this type.
         public static final int fragment_schedule=0x7f030004;
         public static final int month=0x7f030005;
         public static final int week=0x7f030006;
+=======
+        public static final int ic_action_about=0x7f020000;
+        public static final int ic_action_help=0x7f020001;
+        public static final int ic_action_overflow=0x7f020002;
+        public static final int ic_action_settings=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+    }
+    public static final class id {
+        public static final int action_about=0x7f080003;
+        public static final int action_overflow=0x7f080004;
+        public static final int action_settings=0x7f080002;
+        public static final int activity_main=0x7f080000;
+        public static final int description=0x7f080001;
+        public static final int menu_about=0x7f080005;
+    }
+    public static final class layout {
+        public static final int activity_main=0x7f030000;
+        public static final int fragment_exercises=0x7f030001;
+        public static final int fragment_library=0x7f030002;
+        public static final int fragment_schedule=0x7f030003;
+>>>>>>> Stashed changes
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int popup=0x7f080001;
+        public static final int main=0x7f070000;
+        public static final int popup=0x7f070001;
     }
     public static final class string {
-        public static final int app_name=0x7f060003;
-        public static final int day_name_format=0x7f060000;
-        public static final int fragment_exercises=0x7f06000a;
-        public static final int fragment_library=0x7f06000b;
-        public static final int fragment_schedule=0x7f060009;
-        public static final int invalid_date=0x7f060001;
-        public static final int menu_about=0x7f060008;
-        public static final int menu_help=0x7f060007;
-        public static final int menu_overflow=0x7f060006;
-        public static final int menu_settings=0x7f060005;
-        public static final int month_name_format=0x7f060002;
-        public static final int title_main_activity=0x7f060004;
+        public static final int app_name=0x7f050000;
+        public static final int fragment_exercises=0x7f050007;
+        public static final int fragment_library=0x7f050008;
+        public static final int fragment_schedule=0x7f050006;
+        public static final int menu_about=0x7f050005;
+        public static final int menu_help=0x7f050004;
+        public static final int menu_overflow=0x7f050003;
+        public static final int menu_settings=0x7f050002;
+        public static final int title_main_activity=0x7f050001;
     }
     public static final class style {
-        public static final int ActionBarTheme=0x7f070006;
+        public static final int ActionBarTheme=0x7f060002;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -156,124 +109,10 @@ containing a value of this type.
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070004;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070005;
-        public static final int CalendarCell=0x7f070001;
-        public static final int CalendarCell_CalendarDate=0x7f070003;
-        public static final int CalendarCell_DayHeader=0x7f070002;
-        public static final int CalendarTitle=0x7f070000;
+        public static final int AppTheme=0x7f060001;
     }
-    public static final class styleable {
-        /** Attributes that can be used with a calendar_cell.
-           <p>Includes the following attributes:</p>
-           <table>
-           <colgroup align="left" />
-           <colgroup align="left" />
-           <tr><th>Attribute</th><th>Description</th></tr>
-           <tr><td><code>{@link #calendar_cell_state_current_month com.hva.boxlabapp:state_current_month}</code></td><td></td></tr>
-           <tr><td><code>{@link #calendar_cell_state_range_first com.hva.boxlabapp:state_range_first}</code></td><td></td></tr>
-           <tr><td><code>{@link #calendar_cell_state_range_last com.hva.boxlabapp:state_range_last}</code></td><td></td></tr>
-           <tr><td><code>{@link #calendar_cell_state_range_middle com.hva.boxlabapp:state_range_middle}</code></td><td></td></tr>
-           <tr><td><code>{@link #calendar_cell_state_selectable com.hva.boxlabapp:state_selectable}</code></td><td></td></tr>
-           <tr><td><code>{@link #calendar_cell_state_today com.hva.boxlabapp:state_today}</code></td><td></td></tr>
-           </table>
-           @see #calendar_cell_state_current_month
-           @see #calendar_cell_state_range_first
-           @see #calendar_cell_state_range_last
-           @see #calendar_cell_state_range_middle
-           @see #calendar_cell_state_selectable
-           @see #calendar_cell_state_today
-         */
-        public static final int[] calendar_cell = {
-            0x7f010000, 0x7f010001, 0x7f010002, 0x7f010003,
-            0x7f010004, 0x7f010005
-        };
-        /**
-          <p>This symbol is the offset where the {@link com.hva.boxlabapp.R.attr#state_current_month}
-          attribute's value can be found in the {@link #calendar_cell} array.
-
-
-          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
-<p>This may also be a reference to a resource (in the form
-"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
-theme attribute (in the form
-"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
-containing a value of this type.
-          @attr name com.hva.boxlabapp:state_current_month
-        */
-        public static final int calendar_cell_state_current_month = 1;
-        /**
-          <p>This symbol is the offset where the {@link com.hva.boxlabapp.R.attr#state_range_first}
-          attribute's value can be found in the {@link #calendar_cell} array.
-
-
-          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
-<p>This may also be a reference to a resource (in the form
-"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
-theme attribute (in the form
-"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
-containing a value of this type.
-          @attr name com.hva.boxlabapp:state_range_first
-        */
-        public static final int calendar_cell_state_range_first = 3;
-        /**
-          <p>This symbol is the offset where the {@link com.hva.boxlabapp.R.attr#state_range_last}
-          attribute's value can be found in the {@link #calendar_cell} array.
-
-
-          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
-<p>This may also be a reference to a resource (in the form
-"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
-theme attribute (in the form
-"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
-containing a value of this type.
-          @attr name com.hva.boxlabapp:state_range_last
-        */
-        public static final int calendar_cell_state_range_last = 5;
-        /**
-          <p>This symbol is the offset where the {@link com.hva.boxlabapp.R.attr#state_range_middle}
-          attribute's value can be found in the {@link #calendar_cell} array.
-
-
-          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
-<p>This may also be a reference to a resource (in the form
-"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
-theme attribute (in the form
-"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
-containing a value of this type.
-          @attr name com.hva.boxlabapp:state_range_middle
-        */
-        public static final int calendar_cell_state_range_middle = 4;
-        /**
-          <p>This symbol is the offset where the {@link com.hva.boxlabapp.R.attr#state_selectable}
-          attribute's value can be found in the {@link #calendar_cell} array.
-
-
-          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
-<p>This may also be a reference to a resource (in the form
-"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
-theme attribute (in the form
-"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
-containing a value of this type.
-          @attr name com.hva.boxlabapp:state_selectable
-        */
-        public static final int calendar_cell_state_selectable = 0;
-        /**
-          <p>This symbol is the offset where the {@link com.hva.boxlabapp.R.attr#state_today}
-          attribute's value can be found in the {@link #calendar_cell} array.
-
-
-          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
-<p>This may also be a reference to a resource (in the form
-"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
-theme attribute (in the form
-"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
-containing a value of this type.
-          @attr name com.hva.boxlabapp:state_today
-        */
-        public static final int calendar_cell_state_today = 2;
-    };
 }
