e8bf4b18d517cf760a88987a86eb840aaca51145
Bjorn Water <bjorn.water@gmail.com>
2013-11-04 15:58:26 +0100

    - You can now read the sensor data from the shimmers!!!

--- BoxLabApp/src/com/hva/boxlabapp/exercises/Exercise3DActivity.java
+++ BoxLabApp/src/com/hva/boxlabapp/exercises/Exercise3DActivity.java
@@ -1,5 +1,11 @@
 package com.hva.boxlabapp.exercises;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
@@ -11,63 +17,76 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.hva.boxlabapp.R;
 import com.hva.boxlabapp.gdx.Exercise3DObject;
 import com.hva.boxlabapp.gdx.Exercise3DHandler;
+import com.hva.boxlabapp.model.SensorDevice;
 import com.hva.boxlabapp.shimmer.driver.ShimmerHandler;
 
-public class Exercise3DActivity extends AndroidApplication implements Exercise3DHandler {
-	
+public class Exercise3DActivity extends AndroidApplication implements
+		Exercise3DHandler {
+
 	private ShimmerHandler hip;
 	private ShimmerHandler thigh;
 	private ShimmerHandler shin;
-	
-    @Override 
-    public void onCreate (Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        // Start the handler. We need to do something with this when the pause and 
-        // destroy events happen.
-        hip = new ShimmerHandler();
-        thigh = new ShimmerHandler();
-        shin = new ShimmerHandler();
-        
-        // Initialize
-//        hip.init(device, this);
-//        thigh.init(device, this);
-//        shin.init(device, this);
-        
-        // Also we need to do something to distinguish between them.
-        
-        LinearLayout layout = new LinearLayout(this);
-        
-        // Dimensions
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int width = size.x /2;
-        int height = size.y;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        View contentView = LayoutInflater.from(this).inflate(R.layout.exercise_3d_content, null);
-        layout.addView(contentView, width, height);
-       
-        final Exercise3DObject ex = new Exercise3DObject(this);
-        View gameView = initializeForView(ex, false);
-        layout.addView(gameView, width, height);
-        
-        setContentView(layout);
-        
-    }
+		// Start the handler. We need to do something with this when the pause
+		// and
+		// destroy events happen.
+		hip = new ShimmerHandler();
+		SensorDevice device = new SensorDevice(1, "Test",
+				SensorDevice.Type.SHIMMER_2R, "hip");
+		BluetoothAdapter defaultAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (defaultAdapter != null) {
+			Iterator<BluetoothDevice> iterator = defaultAdapter
+					.getBondedDevices().iterator();
+			while (iterator.hasNext()) {
+				BluetoothDevice next = iterator.next();
+				device.setMac(next.getAddress());
+			}
+		}
+
+		// Initialize
+		hip.init(device, this);
+		// thigh.init(device, this);
+		// shin.init(device, this);
+
+		// Also we need to do something to distinguish between them.
+
+		LinearLayout layout = new LinearLayout(this);
+
+		// Dimensions
+		Display display = getWindowManager().getDefaultDisplay();
+		Point size = new Point();
+		display.getSize(size);
+		int width = size.x / 2;
+		int height = size.y;
+
+		View contentView = LayoutInflater.from(this).inflate(
+				R.layout.exercise_3d_content, null);
+		layout.addView(contentView, width, height);
+
+		final Exercise3DObject ex = new Exercise3DObject(this);
+		View gameView = initializeForView(ex, false);
+		layout.addView(gameView, width, height);
+
+		setContentView(layout);
+
+	}
 
 	@Override
 	public int[][] getAccel() {
 		// 3 sensors sending 3 outputs
 		int[][] data = new int[3][3];
-		data[0] = hip.readSensors();
-		data[1] = thigh.readSensors();
-		data[2] = shin.readSensors();
+		// data[0] = hip.readSensors();
+		// data[1] = thigh.readSensors();
+		// data[2] = shin.readSensors();
 		return data;
 	}
-	
+
 	@Override
-	public float[][] getGyro(){
+	public float[][] getGyro() {
 		float[][] gyro = {};
 		return gyro;
 	}
--- BoxLabApp/src/com/hva/boxlabapp/shimmer/driver/ShimmerHandler.java
+++ BoxLabApp/src/com/hva/boxlabapp/shimmer/driver/ShimmerHandler.java
@@ -11,21 +11,25 @@ import android.util.Log;
 
 public class ShimmerHandler extends Handler {
 	private static final String TAG = "ShimmerHandler";
-	private int[] data;
-	
-	public ShimmerHandler(){
-		data = new int[3];
+	private double[] data;
+	private Shimmer shimmer;
+	public ShimmerHandler() {
+		data = new double[6];
 		data[0] = 0;
 		data[1] = 0;
 		data[2] = 0;
- 	}
-	
-	public void init (SensorDevice device, Context context){
-		Shimmer shimmerDevice = new Shimmer(context, this,device.getName(),10,1, 4, Shimmer.SENSOR_ACCEL, false);
-		shimmerDevice.connect(device.getMac(),"default"); //address is just a string name, any name can be used
-    	shimmerDevice.setgetdatainstruction("a"); // ???
-    	shimmerDevice.writeEnabledSensors(Shimmer.SENSOR_ACCEL);
-    	shimmerDevice.startStreaming();
+		data[3] = 0;
+		data[4] = 0;
+		data[5] = 0;
+	}
+
+	public void init(SensorDevice device, Context context) {
+		Shimmer shimmerDevice = new Shimmer(context, this, device.getName(),
+				10, 1, 4, Shimmer.SENSOR_ACCEL, false);
+		shimmerDevice.connect(device.getMac(), "default"); // address is just a
+															// string name, any
+		this.shimmer = shimmerDevice;													// name can be used
+		
 	}
 
 	@Override
@@ -36,6 +40,9 @@ public class ShimmerHandler extends Handler {
 			switch (msg.arg1) {
 			case Shimmer.MSG_STATE_FULLY_INITIALIZED:
 				Log.i(TAG, "Shimmer fully initialized");
+				 shimmer.setgetdatainstruction("a"); // ???
+				 shimmer.writeEnabledSensors(Shimmer.SENSOR_ACCEL | Shimmer.SENSOR_GYRO);
+				 shimmer.startStreaming();
 				break;
 			case Shimmer.STATE_CONNECTING:
 				Log.i(TAG, "Shimmer connecting");
@@ -43,6 +50,7 @@ public class ShimmerHandler extends Handler {
 			case Shimmer.STATE_NONE:
 				Log.i(TAG, "Shimmer not connected");
 				break;
+
 			}
 			break;
 		case Shimmer.MESSAGE_READ:
@@ -51,29 +59,40 @@ public class ShimmerHandler extends Handler {
 				ObjectCluster objectCluster = (ObjectCluster) msg.obj;
 
 				String[] sensorName = new String[0];
-				sensorName = new String[3]; // for x y and z axis
+				sensorName = new String[6]; // for x y and z axis
 				sensorName[0] = "Accelerometer X";
 				sensorName[1] = "Accelerometer Y";
 				sensorName[2] = "Accelerometer Z";
-				for (int i = 0; i < 3; i++) {
+				sensorName[3] = "Gyroscope X";
+    			sensorName[4] = "Gyroscope Y";
+    			sensorName[5] = "Gyroscope Z";
+				for (int i = 0; i < sensorName.length; i++) {
 					Collection<FormatCluster> ofFormats = objectCluster.mPropertyCluster
 							.get(sensorName[i]);
 					FormatCluster formatCluster = ((FormatCluster) ObjectCluster
 							.returnFormatCluster(ofFormats, "CAL"));
 					if (formatCluster != null) {
-						data[i] = (int) ((FormatCluster) ObjectCluster
-								.returnFormatCluster(ofFormats, "RAW")).mData;
+						data[i] = formatCluster.mData;
+						Log.i(TAG, "Data calibrated for sensorname " + sensorName[i]+"  = " + data[i]);
+
 					}
 				}
+
 			}
 
 			break;
-
+		case Shimmer.MESSAGE_ACK_RECEIVED:
+			Log.i(TAG,"Message state = MESSAGE_ACK_RECEIVED");
+			
+			break;
+		case Shimmer.MESSAGE_INQUIRY_RESPONSE:
+			Log.i(TAG, "Messagae state = MESSAGE_INQUIRY_RESPONSE");
 		}
 		
+
 	}
-	
-	public int[] readSensors(){
+
+	public double[] readSensors() {
 		return data;
 	}
 }
