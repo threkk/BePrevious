aa9e7ac6b9287facce6c647ab47628c48112b247
terraego <terraego@gmail.com>
2014-01-06 14:11:47 +0100

    - seperated the uploading and downloading synchronization into two seperate files
     - implemented the uploading of device states

---
+++ boxlab-server/modules/synchronization/upload.js
@@ -0,0 +1,140 @@
+var async = require('async');
+var fs = require('fs');
+var moment = require('moment');
+
+var logger = require('../logging').getLogger('io');
+var identification = require('../identification');
+var paths = require('../paths');
+
+var upload_file_path = '/boxlab/api/' + identification.getIdentity() + '/devices/state';
+
+/**
+ * uploads the currently registered devices to the master server
+ * 
+ * @param callback
+ */
+function uploadDevices(client, callback) {
+	logger.debug('need to send devices to server');
+	logger.debug('NOT YET IMPLEMENTED');
+	callback();
+}
+
+/**
+ * uploads all the json files to the master server
+ * 
+ * @param callback
+ */
+function uploadFiles(client, callback) {
+	_listUploadFiles(function(err, files) {
+		logger.debug("files to upload: " + JSON.stringify(files));
+		async.eachSeries(files, function(file, fn) {
+			_uploadFile(client, file, fn);
+		}, callback);
+	});
+}
+
+/**
+ * lists the files that need to be parsed and uploaded to the master server
+ * 
+ * @param callback(error,result)
+ */
+function _listUploadFiles(callback) {
+	// read all files in the data/out directory
+	function readFiles(fn) {
+		fs.readdir(paths.data_out, fn)
+	}
+
+	// filter out files that do not need to be sent
+	function filterFiles(files, fn) {
+		async.filter(files, function(filename, fn) {
+			var keep = false;
+			var ext = filename.split('.').pop();
+
+			if (ext == 'json') {
+				// we want to upload files that are json files
+				keep = true;
+
+				var name = filename.substr(0, filename.length - 5);
+				var todayName = moment().format('YYYY-MM-DD');
+				if (name == todayName) {
+					// dont want to upload the file that were still modifying
+					// e.g. files that are of this date
+					keep = false;
+				}
+			}
+
+			fn(keep);
+		}, function(filtered) {
+			fn(null, filtered);
+		});
+	}
+
+	// prepend the correct path to the filenames
+	function sanitizeFiles(files, fn) {
+		var sanitized = [];
+		for ( var key in files) {
+			var filename = files[key];
+			sanitized.push(paths.relative(paths.data_out, filename));
+		}
+		fn(null, sanitized);
+	}
+
+	async.waterfall([ readFiles, filterFiles, sanitizeFiles ], callback);
+}
+
+function _uploadFile(client, file, callback) {
+	function doUpload(fn) {
+		_parseFile(file, function(err, data) {
+			if (err) {
+				return fn(err);
+			}
+
+			client.post(upload_file_path, data, function(err, req, res) {
+				fn(err);
+			})
+		})
+	}
+
+	function doRename(fn) {
+		fs.rename(file, file + '.sent', callback)
+	}
+
+	async.series([ doUpload, doRename ], callback);
+}
+
+/**
+ * parses the given file to a json array
+ * 
+ * @param file
+ *            the file to parse
+ * @param callback(error,
+ *            result)
+ */
+function _parseFile(file, callback) {
+	fs.readFile(file, function(err, data) {
+		if (err) {
+			return callback(err);
+		}
+
+		// data is an object, not a string..
+		var formatted = '' + data;
+		// split the data into an array per line
+		formatted = formatted.split('\r\n');
+
+		var parsed = [];
+		for ( var key in formatted) {
+			if (formatted[key].length < '{}'.length) {
+				continue;
+			}
+
+			parsed.push(JSON.parse(formatted[key]));
+		}
+
+		callback(null, parsed);
+	});
+}
+
+module.exports = {
+	uploadDevices : uploadDevices,
+	uploadFiles : uploadFiles
+}
--- boxlab-server/modules/synchronizer.js
+++ boxlab-server/modules/synchronizer.js
@@ -1,22 +1,16 @@
-var fs = require('fs');
-
 var async = require('async');
 var restify = require('restify');
-var moment = require('moment');
 
 var configuration = require('../configuration.js');
 
 var logger = require('./logging').getLogger('io');
-var paths = require('./paths');
-var connector = require('internetconnector').connector;
-var identification = require('./identification');
+var connector = require('./internetconnector').connector;
 var localDB = require('./localdatabase').localDB;
 
+var downloader = require('./synchronization/download');
+var uploader = require('./synchronization/upload');
 
-var messages_file = paths.relative(paths.data_in, 'messages.json');
-var entries_file = paths.relative(paths.data_in, 'entries.json');
-
-var restClient = restify.createJsonClient({
+var client = restify.createJsonClient({
 	url : configuration.master_host,
 	version : '*'
 });
@@ -28,12 +22,11 @@ var restClient = restify.createJsonClient({
  * @param callback
  */
 function synchronize(callback) {
-	connector.connect(function(err) {
-		if (err) {
-			logger.error('failed to connect to the internet: ' + JSON.stringify(err));
-			return callback(err);
-		}
+	function doConnect(fn) {
+		connector.connect(fn);
+	}
 
+	function doSync(fn) {
 		logger.info('synchronizing with master server');
 		var start = +new Date();
 		async.series([ downloadFiles, uploadFiles ], function(err) {
@@ -42,14 +35,18 @@ function synchronize(callback) {
 			}
 			callback(err);
 		});
+	}
 
+	function doDisconnect(fn) {
 		connector.disconnect(function(err) {
 			if (err) {
-				return logger.error('failed to disconnect to the internet');
+				logger.error('failed to disconnect to the internet');
 			}
-			callback();
+			fn();
 		});
-	})
+	}
+
+	async.series([ doConnect, doSync, doDisconnect ], callback);
 }
 
 /**
@@ -61,211 +58,35 @@ function synchronize(callback) {
  */
 function downloadFiles(callback) {
 	logger.info('downloading data from master server');
-	async.series([ _downloadMessages, _downloadEntries ], callback);
+
+	function doDownloadMessages(fn) {
+		downloader.downloadMessages(client, fn);
+	}
+
+	function doDownloadEntries(fn) {
+		downloader.downloadEntries(client, fn);
+	}
+
+	async.series([ doDownloadMessages, doDownloadEntries ], callback);
 }
 
 function uploadFiles(callback) {
 	logger.info('uploading data to master server (not yet implemented)');
-	async.series([ _uploadDevices, _uploadFiles ], callback);
-}
 
-/**
- * downloads the messages from the master server
- * 
- * @param callback
- */
-function _downloadMessages(callback) {
-	var query = {
-		from : localDB.getLastSyncDate()
-	};
-
-	var path = '/boxlab/api/' + identification.getIdentity() + '/messages';
-	var query = '?from=' + localDB.getLastSyncDate();
-	restClient.get(path + query, function(err, req, res, data) {
-		if (err) {
-			return callback(err);
-		}
-
-		if (data.length > 0) {
-			logger.info('%d messages were downloaded', data.length);
-		} else {
-			logger.info('no new messages were found');
-		}
-
-		_appendFile(messages_file, data, callback);
-	});
-}
-
-/**
- * downloads the entries from the master server
- * 
- * @param callback
- */
-function _downloadEntries(callback) {
-	var query = {
-		from : localDB.getLastSyncDate()
-	};
-
-	var path = '/boxlab/api/' + identification.getIdentity() + '/entries';
-	var query = '?from=' + localDB.getLastSyncDate();
-	restClient.get(path + query, function(err, req, res, data) {
-		if (err) {
-			return callback(err);
-		}
-		if (data.length > 0) {
-			logger.info('%d exercise entries were downloaded', data.length);
-		} else {
-			logger.info('no new exercise entries were downloaded');
-		}
-
-		_appendFile(entries_file, data, callback);
-	});
-}
-
-/**
- * appends the found data to the given file
- * 
- * @param file
- *            the file to append to
- * @param data
- *            the array that needs to be appended to the file
- * @param callback
- */
-function _appendFile(file, data, callback) {
-	var lines = '';
-	for ( var key in data) {
-		lines += JSON.stringify(data[key]) + '\r\n';
+	function doUploadDevices(fn) {
+		uploader.uploadDevices(client, fn);
 	}
 
-	fs.appendFile(file, lines, callback);
-}
-
-/**
- * uploads the currently registered devices to the master server
- * 
- * @param callback
- */
-function _uploadDevices(callback) {
-	logger.debug('need to send devices to server', JSON.stringify(stateUpdate));
-	logger.debug('NOT YET IMPLEMENTED');
-	callback();
-}
-
-/**
- * uploads all the json files to the master server
- * 
- * @param callback
- */
-function _uploadFiles(callback) {
-	_listUploadFiles(function(err, files) {
-		logger.debug("files to upload: " + JSON.stringify(files));
-		async.eachSeries(files, function(file, fn) {
-			_parseFile(file, function(err, data) {
-				if (err) {
-					return fn(err);
-				}
-
-				_uploadParsedFile(data, fn);
-			})
-		}, callback);
-	});
-}
-
-/**
- * parses the given file to a json array
- * 
- * @param file
- *            the file to parse
- * @param callback(error,
- *            result)
- */
-function _parseFile(file, callback) {
-	fs.readFile(file, function(err, data) {
-		if (err) {
-			return callback(err);
-		}
-
-		// data is an object, not a string..
-		var formatted = '' + data;
-		// split the data into an array per line
-		formatted = formatted.split('\r\n');
-
-		var parsed = [];
-		for ( var key in formatted) {
-			if (formatted[key].length < '{}'.length) {
-				continue;
-			}
-
-			parsed.push(JSON.parse(formatted[key]));
-		}
-
-		callback(null, parsed);
-	});
-}
-
-/**
- * uploads the given parsed data to the master server
- * 
- * @param data
- * @param callback
- */
-function _uploadParsedFile(data, callback) {
-	async.eachSeries(data, function(stateUpdate, fn) {
-		logger
-				.debug('need to send "%s" to server', JSON
-						.stringify(stateUpdate));
-		logger.debug('NOT YET IMPLEMENTED');
-		fn();
-	}, callback)
-}
-
-/**
- * lists the files that need to be parsed and uploaded to the master server
- * 
- * @param callback(error,result)
- */
-function _listUploadFiles(callback) {
-	// read all files in the data/out directory
-	function readFiles(fn) {
-		fs.readdir(paths.data_out, fn)
+	function doUploadFiles(fn) {
+		uploader.uploadFiles(client, fn);
 	}
 
-	// filter out files that do not need to be sent
-	function filterFiles(files, fn) {
-		async.filter(files, function(filename, fn) {
-			var keep = false;
-			var ext = filename.split('.').pop();
-
-			if (ext == 'json') {
-				// we want to upload files that are json files
-				keep = true;
-
-				var name = filename.substr(0, filename.length - 5);
-				var todayName = moment().format('YYYY-MM-DD');
-				if (name == todayName) {
-					// dont want to upload the file that were still modifying
-					// e.g. files that are of this date
-					keep = false;
-				}
-			}
-
-			fn(keep);
-		}, function(filtered) {
-			fn(null, filtered);
-		});
-	}
-
-	// prepend the correct path to the filenames
-	function sanitizeFiles(files, fn) {
-		var sanitized = [];
-		for ( var key in files) {
-			var filename = files[key];
-			sanitized.push(paths.relative(paths.data_out, filename));
-		}
-		fn(null, sanitized);
-	}
-
-	async.waterfall([ readFiles, filterFiles, sanitizeFiles ], callback);
+	async.series([ doUploadDevices, doUploadFiles ], callback);
 }
 
 module.exports.synchronize = synchronize;
+module.exports.synchronize(function(err) {
+	console.log('finished');
+	if (err)
+		console.log('err: ' + JSON.stringify(err));
+});
