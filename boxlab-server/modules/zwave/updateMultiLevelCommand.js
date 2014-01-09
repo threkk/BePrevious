585a439037bddc8847951a58466a1d89809a40e7
terraego <terraego@gmail.com>
2014-01-09 17:51:47 +0100

    - added a trigger for multilevel values to write the state to file (temperature, luminescence)

--- boxlab-server/modules/zwave/updateMultiLevelCommand.js
+++ boxlab-server/modules/zwave/updateMultiLevelCommand.js
@@ -1,6 +1,7 @@
 var async = require('async');
 var logger = require('../logging').getLogger('client');
 var localDB = require('../localdatabase').localDB;
+var writer = require('../writer').writer;
 var client = require('./client');
 
 module.exports.updateMultiLevelCommand = function(deviceManager, callback) {
@@ -23,7 +24,29 @@ function updateMultiLevel(device, callback) {
 			return callback();
 		}
 
-		device.updateState(parseResult(nodeId, json), callback);
+		updateDevice(device, parseResult(nodeId, json), callback);
+	});
+}
+
+function updateDevice(device, stateUpdate, callback) {
+	device.updateState(stateUpdate.state, function(err, dirty, currentState) {
+		if (err) {
+			logger.error("failed to update multilevel state: " + JSON.stringify(err));
+			return callback(err);
+		}
+
+		if (dirty) {
+			var message = {
+				nodeId : device.data.id,
+				timestamp : stateUpdate.timestamp,
+				state : currentState
+			};
+			writer.write(message, function(err, status) {
+				logger.debug('wrote to file: ' + JSON.stringify(message));
+			});
+		}
+
+		callback();
 	});
 }
 
@@ -39,17 +62,24 @@ function parseResult(nodeId, json) {
 			var updateTime = instance.val.updateTime;
 			var value = instance.val.value;
 
+			var name = normalizeSensorName(sensorType);
+
 			// adjust the read value with the stored temperature offset
-			if (sensorType == 'Temperature') {
+			if (name == 'temperature') {
 				value = Math.round(value);
 				value = value + localDB.getTempOffset(nodeId);
 			}
 
-			// normalize the name
-			var name = sensorType.charAt(0).toLowerCase() + sensorType.slice(1);
-
-			result[name] = value;
+			result.timestamp = updateTime;
+			result.state[name] = value;
 		}
 	}
+	return result;
+}
+
+function normalizeSensorName(name) {
+	// decapitalize the first character
+	var result = name.charAt(0).toLowerCase() + name.slice(1);
+
 	return result;
 }
